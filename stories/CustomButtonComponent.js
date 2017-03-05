4a62f6dcb56df29b6b5755e527dd38c4b8f93cef
arncet <arnaud.cetoute@hotmail.fr>
2017-03-05 02:55:59 +0100

    Release 1.0.4

---
+++ stories/CustomButtonComponent.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import { action } from '@kadira/storybook'
+import Readmore from '../src/index'
+import LoremHTML from './LoremHTML'
+
+const CustomButtonHTML = () => (
+  <Readmore type={CustomComponent}>
+    <LoremHTML title='Custom Button (component)' />
+  </Readmore>
+)
+
+const CustomComponent = ({ children, ...props }) => (
+  <div {...props} style={{ color: '#FFF', backgroundColor: 'blue', padding: '5px 10px', textAlign: 'center' }}>
+    {children}
+  </div>
+)
+
+export default CustomButtonHTML
---
+++ stories/CustomButtonHTML.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { action } from '@kadira/storybook'
+import Readmore from '../src/index'
+import LoremHTML from './LoremHTML'
+
+const CustomButtonHTML = () => (
+  <Readmore type='a'>
+    <LoremHTML title='Custom Button (HTML)' />
+  </Readmore>
+)
+
+export default CustomButtonHTML
